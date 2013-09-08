86579179ae2aab8022d6c9d10aa7c849edd527fe
John Brady <bradyjoh@grinnell.edu>
2013-09-08 13:39:18 -0500

    More testing
    There are no examples that I can find of inputs, outputs, and
    procedures, so just setting up the program (before the methods) is
    proving difficult

--- src/twoA.java
+++ src/twoA.java
@@ -2,7 +2,11 @@
 public class twoA {
     
     public static void main(String[] args) {
-	       System.out.print("I am the 1337 h4x0r. Phear me!");
+	       System.out.print("I am the 1337 h4x0r. Phear me!" + 7 + 4);
 	   } // main(String[])
+    public static
+    int square(int x) throws Exception{
+	return x * x;
+    }
 
-}
+} // public class twoA
