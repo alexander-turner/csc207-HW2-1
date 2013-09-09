b031bcacabe895e357ece20b9e210e14198f46d9
John Brady <bradyjoh@grinnell.edu>
2013-09-09 14:28:14 -0500

    draft of methods is done

--- src/edu/grinnell/csc207/bradyjoh/hw2/TwoA.java
+++ src/edu/grinnell/csc207/bradyjoh/hw2/TwoA.java
@@ -1,55 +1,68 @@
 package edu.grinnell.csc207.bradyjoh.hw2;
 
 public class TwoA {
-    
-  
+
+
     public static boolean isMultiple(long a, long b) 
     { if ((a/b) == (int)(a/b)) //integer test from http://stackoverflow.com/questions/5502548/checking-if-a-number-is-an-integer-in-java
 	return true;
     else
 	return false;
-    
-   /* second version:
+
+    /* second version:
      int i;
     	    for(i = 1; b*i < a; i++);
             return (b*i == a);  
-    */
-    
+     */
+
     } // isMultiple
-    
+
     public static boolean isOdd(int i)
     {int half_i = i << 1;
-	    return (i != 0) && (i == (half_i >> 1));
-	
+    return (i != 0) && (i == (half_i >> 1));
+
     }
-    
+
     public static int oddSumTo(int n)
     {
-    int sum = 0;
-	    for(int i = 1; i < n; i+=2)
-	        sum += i;
-	    return sum;
+	int sum = 0;
+	for(int i = 1; i < n; i+=2)
+	    sum += i;
+	return sum;
     }
-    
+
     public static boolean isOddProd (int[] ints)
-    { return true;
-    
+    { 
+	if(ints.length <= 1) {
+
+	    return false;
+	}
+
+	else {
+	    for(int x = 0; x<(ints.length-1);x++)
+	    {
+		if(TwoA.isOdd((ints[x]*ints[x+1])))
+		    return true;
+	    }
+	    return false;
+	}
+
     }
-    
+
     public static boolean allDistinct(int[] ints){
 	if(ints.length <= 1) {
-		return false;
+	    return false;
 	}
 	else {
 	    for(int x = 0; x<(ints.length-1);x++)
 	    {int num=ints[x];
-	    	
-		    for(int y=x+1;y<(ints.length-1);y++)
-		    {
-			if (num==ints[y])
-				return true;
-		    }
-		
+
+	    for(int y=x+1;y<(ints.length-1);y++)
+	    {
+		if (num==ints[y])
+		    return true;
+	    }
+
 	    } //for
 	    return false;
 	}
@@ -57,7 +70,7 @@ public class TwoA {
 
     public static int[] reverseInts(int[] ints){
 	if(ints.length <= 1) {
-		return ints;
+	    return ints;
 	}
 	else {
 	    int i = 0;
@@ -72,7 +85,7 @@ public class TwoA {
 	    }
 	    return ints;
 	}
-	
+
     } //reverseInts - idea from http://stackoverflow.com/questions/2137755/how-do-i-reverse-an-int-array-in-java
 }
 
