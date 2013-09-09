6a790de1f8d91b3df3e32c4b051ab845d9259119
John Brady <bradyjoh@grinnell.edu>
2013-09-09 18:54:56 -0500

    beginning part C

--- src/edu/grinnell/csc207/bradyjoh/hw2/TwoC.java
+++ src/edu/grinnell/csc207/bradyjoh/hw2/TwoC.java
@@ -2,4 +2,37 @@ package edu.grinnell.csc207.bradyjoh.hw2;
 
 public class TwoC {
 
+    // Observers
+    public static int numerator() {
+	return 0; // STUB
+    } // numerator
+    
+    public static int denominator() {
+	 return 0; // STUB
+    } // denominator
+    
+    public static int string() {
+	return 0; // STUB
+    } // string
+    
+    public static int reciprocal() {
+	return 0; // STUB
+    } // reciprocal
+    
+    public static int decimalRep() {
+	return 0; // STUB
+    } // decimalRep
+    
+    public static int rationalRep() {
+	return 0; // STUB
+    } // rationalRep
+    
+    // Mutators
+    public static void numeratorSet() {
+	// STUB
+    } // numeratorSet
+    public static void denominatorSet() {
+	// STUB
+    } // denominatorSet
+    
 }
