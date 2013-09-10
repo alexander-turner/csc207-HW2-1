b0c8605f36fb79576ef7188bf5e98aaa4a69fd8f
John Brady <bradyjoh@grinnell.edu>
2013-09-09 20:26:47 -0500

    fixed B

--- src/edu/grinnell/csc207/bradyjoh/hw2/TwoB.java
+++ src/edu/grinnell/csc207/bradyjoh/hw2/TwoB.java
@@ -4,15 +4,13 @@ public class TwoB {
     /**
      * Compute the average of two integers.  Rounds toward zero if the
      * average is not a whole number.
-     * One issue is when we want the average of Integer.MAX_VALUE, or something that 
-     * would push our sum over Integer.MAX_VALUE. We can at least prevent it 
-     * from reaching MAX_INT if the two numbers would sum to over the value,
-     * or if the numbers are the same.
+     *There would be an issue with the sum of the two numbers going past
+     *Integer.MAX_VALUE. We fix that by storing the intermediate sum as a 
+     *long integer (64 bit) and restore the original 32 bit version for the 
+     *return statement.
      */
     public static int average(int left, int right) {
-	if(left>(Integer.MAX_VALUE/2) && right>(Integer.MAX_VALUE/2)) //alternatively, (left==right)
-	    return -1;
-	else
-        return (left + right) / 2;
+	long bob = (left + right)/2;
+	return (int)bob;	
     } // average(int,int)
 } // class TwoB
