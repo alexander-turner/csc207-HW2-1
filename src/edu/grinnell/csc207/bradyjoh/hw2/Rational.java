9e6a6c48da4871791814193350126d3c8f965fb2
alexander-turner <turneral1@grinnell.edu>
2013-09-09 21:13:20 -0500

    Added more methods and tidied up existing methods.

---
+++ src/edu/grinnell/csc207/bradyjoh/hw2/Rational.java
@@ -0,0 +1,156 @@
+package edu.grinnell.csc207.bradyjoh.hw2;
+
+public class Rational {
+
+
+    /* name:numerator
+     * inputs: none.
+     * output: n, an integer
+     * purpose: finds the numerator of the given rational number
+     * method type: observer
+     */
+    public static int numerator() {
+	return 0; // STUB
+    } // numerator
+    
+    /* name:denominator
+     * inputs: none.
+     * output: d, an integer
+     * purpose: finds the denominator of the given rational number
+     * method type: observer
+     */
+    public static int denominator() {
+	 return 0; // STUB
+    } // denominator
+    
+    /* name:string
+     * inputs: none.
+     * output: str, a string
+     * purpose: gives a string representation of the rational number
+     * method type: observer
+     */
+    public static char[] string() {
+	return 0; // STUB
+    } // string
+    
+    /* name:reciprocal
+     * inputs: none.
+     * output: y, a double
+     * purpose: takes the reciprocal of the rational number
+     * method type: observer
+     */
+    public static double reciprocal() {
+	return 0; // STUB
+    } // reciprocal
+    
+    /* name:decimalRep
+     * inputs: none.
+     * output: y, a double
+     * purpose: returns the decimal representation of the rational number
+     * method type: observer
+     */
+    public static double decimalRep() {
+	return 0; // STUB
+    } // decimalRep
+    
+    /* name: round
+     * inputs: none.
+     * output: none.
+     * purpose: rounds the rational number to the nearest integer
+     * method type: mutator
+     */
+    public static void round(){
+	// STUB
+    } // round
+    
+    /* name: floor
+     * inputs: none.
+     * output: none.
+     * purpose: rounds a rational number down to the previous integer
+     * method type: mutator
+     */
+    public static void floor(){
+	// STUB
+    } // floor
+    
+    /* name: ceiling
+     * inputs: none
+     * output: none
+     * purpose: rounds the rational number up to the next integer
+     * method type: mutator
+     */
+    public static void ceiling(){
+	// STUB
+    } // ceiling
+    
+    /* name: add
+     * inputs: addNum, a Rational
+     * output: none.
+     * purpose: adds addNum to the rational number
+     * method type: mutator
+     */
+    public static void add(Rational addNum){
+	// STUB
+    } // add
+    
+    /* name: multiply
+     * inputs: multiplicand, an integer
+     * output: none.
+     * purpose: multiplies the rational number by the multiplicand
+     * method type: mutator
+     */
+    public static void multiply(int multiplicand){
+	// STUB
+    } // multiply
+    
+    /* name: square
+     * inputs: none.
+     * output: none.
+     * purpose: squares the rational number
+     * method type: mutator
+     */
+    public static void square(){
+	// STUB
+    } // square
+    
+    /* name: expt
+     * inputs: n, an integer
+     * output: none.
+     * purpose: exponentiates the rational number to the nth power
+     * method type: mutator
+     */
+    public static void expt(int n){
+	// STUB
+    } // cube
+  
+    /* name: sqrt
+     * inputs: none.
+     * output: none.
+     * purpose: finds the square root of the rational number
+     * method type: mutator
+     */
+    public static void sqrt(){
+        // STUB
+    } // sqrt
+    
+    /* name: create
+     * inputs: numerator, an integer
+     *         denominator, an integer
+     * output: newRational, a Rational
+     * purpose: create new Rational objects
+     * method type: constructor
+     */
+     public static Rational create(int numerator, int denominator){
+     	return null; // STUB
+     } // create
+    
+    /* name: duplicate
+     * inputs: oldRational, a Rational
+     * output: newRational, a Rational
+     * purpose: duplicate a Rational object
+     * method type: constructor
+     */
+     public static Rational duplicate(Rational oldRational){
+     	return null; // STUB
+     } // duplicate
+}
